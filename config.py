3403368d5c8e3163ae26031fdb97330a4e3fadb1
Rasul Yusupov <ry1961m@gmail.com>
2026-02-16 18:41:26 +0300

    3Commit

---
+++ config.py
@@ -0,0 +1,14 @@
+DBASE = ".\\db\\kadr.db"
+XLS = ".\\xls\\coworkers.xlsx"
+LOG = ".\\log\\bot.log"
+PATH1 = ".\\xls\\"
+PATH2 = ".\\files\\"
+PATH3 = ".\\shab\\"
+TOKEN = '8570973938:AAHa36c5MFrtEicNbzaGCxzDfuCh9CSor20'
+WORKING_START = "08:30:00"
+WORKING_END = "18:30:00"
+FORA = 5
+PRIX = 240
+RADIUS = 60
+superadmin = 139204666
+ADMIN = 139204666
